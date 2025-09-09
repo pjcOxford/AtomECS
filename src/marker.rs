02a3dc18f61946f52db88ada44bf6d97d21e8024
Parv Choudhary <148824509+pjcOxford@users.noreply.github.com>
2025-09-09 22:05:30 +0100

    Improved marker.

--- src/marker.rs
+++ src/marker.rs
@@ -1,7 +1,8 @@
 use bevy::prelude::*;
-use crate::atom::{Atom, Position, Velocity};
+use crate::atom::{Position, Velocity};
 use crate::collisions::CollisionsSet;
 use crate::initiate::NewlyCreated;
+use crate::integrator::AtomECSBatchStrategy;
 
 #[derive(PartialEq, Eq)]
 pub enum WriteOrNot {
@@ -13,6 +14,12 @@ pub struct Marker {
     pub write_status: WriteOrNot
 }
 
+#[derive(Resource)]
+pub struct MarkerConfig {
+    pub pos_range: Vec<(f64, f64)>,
+    pub vel_range: Vec<(f64, f64)>,
+}
+
 pub fn init_marker_system (
     mut commands: Commands,
     query: Query<Entity, With<NewlyCreated>>
@@ -22,49 +29,49 @@ pub fn init_marker_system (
     }
 }
 
-#[derive(Resource)]
-pub struct MarkerConfig {
-    pub pos_range: Vec<(f64, f64)>,
-    pub vel_range: Vec<(f64, f64)>,
+pub fn update_marker_system(
+    mut query: Query<(&Position, &Velocity, &mut Marker)>,
+    config: Res<MarkerConfig>,
+    batch_strategy: Res<AtomECSBatchStrategy>,
+) {
+    query
+        .par_iter_mut()
+        .batching_strategy(batch_strategy.0.clone())
+        .for_each(|(pos, vel, mut marker)| {
+            match marker.write_status {
+                WriteOrNot::Write => {
+                    let mut remove_marker = false;
+                    for i in 0..3 {
+                        if !(pos.pos[i] < config.pos_range[i].1 && pos.pos[i] > config.pos_range[i].0) {
+                            remove_marker = true;
+                        }
+                        if !(vel.vel[i] < config.vel_range[i].1 && vel.vel[i] > config.vel_range[i].0) {
+                            remove_marker = true;
+                        }
+                    }
+                    if remove_marker {
+                        marker.write_status = WriteOrNot::NotWrite;
+                    }
+                },
+                WriteOrNot::NotWrite => {
+                    let mut add_marker = true;
+                    for i in 0..3 {
+                        if !(pos.pos[i] < config.pos_range[i].1 && pos.pos[i] > config.pos_range[i].0) {
+                            add_marker = false;
+                        }
+                        if !(vel.vel[i] < config.vel_range[i].1 && vel.vel[i] > config.vel_range[i].0) {
+                            add_marker = false;
+                        }
+                    }
+                    if add_marker {
+                        marker.write_status = WriteOrNot::Write;
+                    }
+                },
+                _ => ()
+            }
+    });
 }
 
-pub fn update_marker_system(
-    mut query: Query<(Entity, &Position, &Velocity, &mut Marker)>,
-    config: Res<MarkerConfig>,
-) {
-    for (entity, pos, vel, mut marker) in query.iter_mut() {
-        match marker.write_status {
-            WriteOrNot::Write => {
-                let mut remove_marker = false;
-                for i in 0..3 {
-                    if !(pos.pos.x < config.pos_range[i].1 && pos.pos.x > config.pos_range[i].0) {
-                        remove_marker = true;
-                    }
-                    if !(vel.vel.x < config.vel_range[i].1 && vel.vel.x > config.vel_range[i].0) {
-                        remove_marker = true;
-                    }
-                }
-                if remove_marker {
-                    marker.write_status = WriteOrNot::NotWrite;
-                }
-            },
-            WriteOrNot::NotWrite => {
-                let mut add_marker = true;
-                for i in 0..3 {
-                    if !(pos.pos.x < config.pos_range[i].1 && pos.pos.x > config.pos_range[i].0) {
-                        add_marker = false;
-                    }
-                    if !(vel.vel.x < config.vel_range[i].1 && vel.vel.x > config.vel_range[i].0) {
-                        add_marker = false;
-                    }
-                }
-                if add_marker {
-                    marker.write_status = WriteOrNot::Write;
-                }
-            },
-        }
-    }
-}
 
 pub struct MarkerPlugin;
 impl Plugin for MarkerPlugin {
