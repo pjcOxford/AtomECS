abb88528f4ccc2066e9ce98f89a0489e73737b43
Parv Choudhary <148824509+pjcOxford@users.noreply.github.com>
2025-09-09 17:19:44 +0100

    Added marker config resource and a temporary test for file output

--- benches/file_output.rs
+++ benches/file_output.rs
@@ -1,14 +1,14 @@
 extern crate nalgebra;
 extern crate atomecs as lib;
 
-use criterion::{Criterion, criterion_group, criterion_main};
+use criterion::{Criterion, criterion_group, criterion_main, black_box};
 use rand::Rng;
 use nalgebra::Vector3;
 use bevy::prelude::*;
 use lib::atom::{Atom, Position, Velocity};
 use lib::integrator::{Timestep, IntegrationPlugin};
 use lib::output::file::{FileOutputPlugin, Text};
-use lib::marker::{Marker, MarkerPlugin};
+use lib::marker::{Marker, MarkerPlugin, MarkerConfig};
 
 criterion_group!(benches, bench_simulation);
 criterion_main!{benches}
@@ -25,8 +25,12 @@ fn bench_simulation(c: &mut Criterion) {
             app.add_plugins(IntegrationPlugin);
 
             app.insert_resource(Timestep { delta: 1e-7 });
+            app.insert_resource(MarkerConfig {
+                pos_range: vec![(0.5, 0.75), (f64::MIN, f64::MAX), (f64::MIN, f64::MAX)],
+                vel_range: vec![(f64::MIN, f64::MAX), (f64::MIN, f64::MAX), (f64::MIN, f64::MAX)],
+            });
 
-            for _ in 0..100 {
+            for _ in 0..800 {
                 app.world_mut().spawn((
                     Position {
                         pos: Vector3::new(
@@ -37,15 +41,15 @@ fn bench_simulation(c: &mut Criterion) {
                     },
                     Velocity {
                         vel: Vector3::new(
-                            rng.random::<f64>(),
-                            rng.random::<f64>(),
-                            rng.random::<f64>(),
+                            black_box(1.0),    
+                            black_box(0.0),
+                            black_box(0.0),
                         ),
                     },
                     Atom,
                 ));
             }
-            for _i in 0..5 {
+            for _i in 0..300 {
                 app.update();
             }
         })
--- src/marker.rs
+++ src/marker.rs
@@ -5,19 +5,50 @@ use crate::collisions::CollisionsSet;
 #[derive(Component)]
 pub struct Marker;
 
+#[derive(Resource)]
+pub struct MarkerConfig {
+    pub pos_range: Vec<(f64, f64)>,
+    pub vel_range: Vec<(f64, f64)>,
+}
+
 pub fn add_marker_system(
     mut commands: Commands,
     query: Query<(Entity, &Position, &Velocity), With<Atom>>,
+    config: Res<MarkerConfig>,
 ) {
     for (entity, pos, vel) in query.iter() {
-        // if pos.pos.x < 210e-6 && pos.pos.x > 200e-6 {
-        //     commands.entity(entity).insert(Marker);
-        // }
-        // else if pos.pos.x > 210e-6 {
-        //     commands.entity(entity).remove::<Marker>();
-        // }
-        if pos.pos.x > 0.5 {
+        let mut add_marker = true;
+        for i in 0..3 {
+            if !(pos.pos.x < config.pos_range[i].1 && pos.pos.x > config.pos_range[i].0) {
+                add_marker = false;
+            }
+            if !(vel.vel.x < config.vel_range[i].1 && vel.vel.x > config.vel_range[i].0) {
+                add_marker = false;
+            }
+        }
+        if add_marker {
             commands.entity(entity).insert(Marker);
+        }
+    }
+}
+
+pub fn remove_marker_system(
+    mut commands: Commands,
+    query: Query<(Entity, &Position, &Velocity), With<Atom>>,
+    config: Res<MarkerConfig>,
+) {
+    for (entity, pos, vel) in query.iter() {
+        let mut remove_marker = false;
+        for i in 0..3 {
+            if !(pos.pos.x < config.pos_range[i].1 && pos.pos.x > config.pos_range[i].0) {
+                remove_marker = true;
+            }
+            if !(vel.vel.x < config.vel_range[i].1 && vel.vel.x > config.vel_range[i].0) {
+                remove_marker = true;
+            }
+        }
+        if remove_marker {
+            commands.entity(entity).remove::<Marker>();
         }
     }
 }
@@ -25,6 +56,11 @@ pub fn add_marker_system(
 pub struct MarkerPlugin;
 impl Plugin for MarkerPlugin {
     fn build(&self, app: &mut App) {
+        app.insert_resource(MarkerConfig {
+            pos_range: vec![(f64::MIN, f64::MAX), (f64::MIN, f64::MAX), (f64::MIN, f64::MAX)],
+            vel_range: vec![(f64::MIN, f64::MAX), (f64::MIN, f64::MAX), (f64::MIN, f64::MAX)],
+        });
         app.add_systems(PreUpdate, add_marker_system.after(CollisionsSet::WallCollisionSystems));
+        app.add_systems(PreUpdate, remove_marker_system.after(CollisionsSet::WallCollisionSystems));
     }
 }
